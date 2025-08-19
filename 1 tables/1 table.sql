d9e8c2726688254ed4fdf870ef2a14f9b3cd7324
hanochp <hanochpadwa37@gmail.com>
2025-08-19 17:36:02 +0100

    Update 1 table.sql
    add planet to the body table

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -3,7 +3,8 @@ go
 drop table if exists body
 go 
 create table dbo.body(id int not null identity primary key, 
-bodypart varchar(100) not null, 
+  planet varchar(50) not null,
+ bodypart varchar(100) not null, 
 num int null
 )
 
